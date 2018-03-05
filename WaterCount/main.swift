5816cfd00e9039f8ad529a8068ae4d50f88c9bf6
EyreFree <eyrefree@eyrefree.org>
2018-03-05 22:16:46 +0800

    Initial Commit

---
+++ WaterCount/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  WaterCount
+//
+//  Created by EyreFree on 2018/3/5.
+//  Copyright © 2018年 EyreFree. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+

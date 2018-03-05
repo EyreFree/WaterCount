d0b1a5a0b17b0178bd23038cb233c36f0a1c60c9
EyreFree <eyrefree@eyrefree.org>
2018-03-06 01:23:55 +0800

    Finish

--- UnitTests/UnitTests.swift
+++ UnitTests/UnitTests.swift
@@ -20,9 +20,59 @@ class UnitTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testExample0() {
+        let result = input(rects: [(1, 2, 2), (3, 4, 3), (5, 8, 1), (9, 10, 2)])
+        XCTAssert(result.0 == .success && result.1 == 9, "算错了，快来查查！")
+    }
+    
+    func testExample1() {
+        let result = input(rects: [(1, 2, 2), (3, 4, 3), (5, 8, 1), (9, 10, 100)])
+        XCTAssert(result.0 == .success && result.1 == 14, "算错了，快来查查！")
+    }
+    
+    func testExample2() {
+        let result = input(rects: [(1, 2, 2), (3, 4, 3)])
+        XCTAssert(result.0 == .success && result.1 == 2, "算错了，快来查查！")
+    }
+    
+    func testExample3() {
+        let result = input(rects: [(1, 2, 2), (3, 4, 3), (5, 8, 4), (9, 10, 2)])
+        XCTAssert(result.0 == .success && result.1 == 7, "算错了，快来查查！")
+    }
+    
+    func testExample4() {
+        let result = input(rects: [(1, 2, 2)])
+        XCTAssert(result.0 == .tooFewRects && result.1 == -1, "输入错误没检测成功，快来查查！")
+    }
+    
+    func testExample5() {
+        let result = input(rects: [])
+        XCTAssert(result.0 == .tooFewRects && result.1 == -1, "输入错误没检测成功，快来查查！")
+    }
+    
+    func testExample6() {
+        let result = input(rects: [(1, 2, 2), (1, 4, 5), (5, 8, 4), (9, 10, 2)])
+        XCTAssert(result.0 == .errorRect && result.1 == -1, "输入错误没检测成功，快来查查！")
+    }
+    
+    func testExample7() {
+        let result = input(rects: [(1, 2, 2), (3, 4, 100), (5, 8, 1), (9, 10, 2)])
+        XCTAssert(result.0 == .success && result.1 == 9, "算错了，快来查查！")
+    }
+    
+    func testExample8() {
+        let result = input(rects: [(1, 2, 1), (3, 4, 100), (5, 8, 1), (9, 10, 2)])
+        XCTAssert(result.0 == .success && result.1 == 8, "算错了，快来查查！")
+    }
+    
+    func testExample9() {
+        let result = input(rects: [(1, 2, 2), (3, 4, 100), (5, 8, 1), (10, 11, 2)])
+        XCTAssert(result.0 == .success && result.1 == 11, "算错了，快来查查！")
+    }
+    
+    func testExample10() {
+        let result = input(rects: [(1, 2, 1), (3, 4, 1), (5, 8, 1), (9, 10, 1)])
+        XCTAssert(result.0 == .success && result.1 == 3, "算错了，快来查查！")
     }
     
     func testPerformanceExample() {
--- WaterCount/main.swift
+++ WaterCount/main.swift
@@ -8,5 +8,163 @@
 
 import Foundation
 
-print("Hello, World!")
+// 常量
+class Constant {
+    static let MIN_RECTS_COUNT = 2  // 最小 rect 数
+}
 
+// 异常码
+enum ExitCode: Int {
+    case success = 0
+    case tooFewRects = 1
+    case errorRect = 2
+    case unknown = 3
+    
+    static let Desc = [
+        "",
+        "输入 Rects 数量小于 \(Constant.MIN_RECTS_COUNT)",
+        "某 Rect 格式有误",
+        "未知错误"
+    ]
+}
+
+// Rect 结构体
+struct Rect {
+    var x1 = 0
+    var x2 = 1
+    var h = 1
+    
+    init?(x1: Int, x2: Int, h: Int) {
+        if !(x1 > 0 && x2 > x1 && h > 0) {
+            return nil
+        }
+        
+        self.x1 = x1
+        self.x2 = x2
+        self.h = h
+    }
+}
+
+// 某 1x1 区块
+struct Point {
+    var x1 = 0
+    var x2 = 1
+    var y1 = 0
+    var y2 = 1
+    
+    init?(x1: Int, x2: Int, y1: Int, y2: Int) {
+        if !(x1 < x2 && y1 < y2) {
+            return nil
+        }
+        
+        self.x1 = x1
+        self.x2 = x2
+        self.y1 = y1
+        self.y2 = y2
+    }
+}
+
+func input(rects: [(Int, Int, Int)]) -> (ExitCode, Int) {
+    // 1. 输入的实体矩形数据转为结构体数组，同时检测输入是否合法
+    if rects.count < 2 {
+        return (.tooFewRects, -1)
+    }
+    var rectArray = [Rect]()
+    for rect in rects {
+        if let rectSct = Rect(x1: rect.0, x2: rect.1, h: rect.2) {
+            // 如果不为第一个，则需要额外判断与前一个 rect 的关系是否成立
+            if let lastRect = rectArray.last {
+                if !(rectSct.x1 >= lastRect.x2) {
+                    return (.errorRect, -1)
+                }
+            }
+            
+            // 追加到 rect 数组
+            rectArray.append(rectSct)
+        } else {
+            return (.errorRect, -1)
+        }
+    }
+    
+    // 2. 生成热区
+    var hotRange: Rect?
+    if let minX = rectArray.first?.x2, let maxX = rectArray.last?.x1 {
+        var maxH = 0
+        for rectSct in rectArray {
+            if rectSct.h > maxH {
+                maxH = rectSct.h
+            }
+        }
+        hotRange = Rect(x1: minX, x2: maxX, h: maxH)
+    }
+    
+    // 3. 遍历热区
+    guard let range = hotRange else {
+        return (.unknown, -1)
+    }
+    
+    // 某区是否为实体矩形
+    func isPointSolid(_ point: Point) -> Bool {
+        for rectSct in rectArray {
+            if rectSct.h >= point.y2 && rectSct.x1 <= point.x1 && rectSct.x2 >= point.x2 {
+                return true
+            }
+        }
+        return false
+    }
+    // 某区水滴能否驻留
+    func isPointNoLoss(_ point: Point) -> Bool {
+        // 如果是矩形实体则无法驻留
+        if isPointSolid(point) {
+            return false
+        }
+        
+        // 左边有阻挡
+        var markLeft = false
+        for x in (range.x1 - 1) ..< point.x1 {
+            if let testPoint = Point(x1: x, x2: x + 1, y1: point.y1, y2: point.y2) {
+                if isPointSolid(testPoint) {
+                    markLeft = true
+                }
+            }
+        }
+        // 右边有阻挡
+        var markRight = false
+        for x in point.x2 ..< (range.x2 + 1) {
+            if let testPoint = Point(x1: x, x2: x + 1, y1: point.y1, y2: point.y2) {
+                if isPointSolid(testPoint) {
+                    markRight = true
+                }
+            }
+        }
+        return markLeft && markRight
+    }
+
+    var count = 0
+    for x in range.x1 ..< range.x2 {
+        for y in 0 ..< range.h {
+            if let testPoint = Point(x1: x, x2: x + 1, y1: y, y2: y + 1) {
+                if isPointNoLoss(testPoint) {
+                    count += 1
+                }
+            }
+        }
+    }
+    
+    return (.success, count)
+}
+
+// 使用示例
+var result = input(rects: [(1, 2, 2), (3, 4, 3), (5, 8, 1), (9, 10, 2)])
+if .success != result.0 {
+    print(ExitCode.Desc[result.0.rawValue])
+} else {
+    print(result.1)
+}
+
+result = input(rects: [(1, 2, 2), (1, 4, 5), (5, 8, 4), (9, 10, 2)])
+if .success != result.0 {
+    print(ExitCode.Desc[result.0.rawValue])
+} else {
+    print(result.1)
+}
